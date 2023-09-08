2164e97884e56d8f5a85f8c48b107fe9e3373bf2
tan.zhiheng <tan.zhiheng@tecotec.co.jp>
2023-09-08 15:55:51 +0900

    fix response

--- functions/app.py
+++ functions/app.py
@@ -1,5 +1,6 @@
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import serialization
+import json
 
 
 # a function to generate key pairs
@@ -30,4 +31,8 @@ def handler(event, context):
     print(f"event: {event}")
     print(f"context: {context}")
     private_key_pem, public_key_pub = generate_key_pair()
-    return {"private_key_pem": private_key_pem, "public_key_pub": public_key_pub}
+    body = {"private_key_pem": private_key_pem, "public_key_pub": public_key_pub}
+    return {
+        "statusCode": 200,
+        "body": json.dumps(body),
+    }
