333e23ca22d929a685f0b5c60542697ff8342f10
tan.zhiheng <tan.zhiheng@tecotec.co.jp>
2023-10-12 16:58:42 +0900

    api test

--- functions/app2.py
+++ functions/app2.py
@@ -1,12 +1,19 @@
 import requests
 import json
+from jinja2 import Environment, FileSystemLoader
 
 
 def handler(event, context):
     # url = "https://fakestoreapi.com/products"
     # res = requests.get(url)
 
+    env = Environment(
+        loader=FileSystemLoader("templates"), trim_blocks=True, lstrip_blocks=True
+    )
+
+    template = env.get_template("apigateway+lambda.yaml")
+    result = template.render(json.loads(event.body))
     return {
         "statusCode": 200,
-        "body": json.dumps({"event": event}),
+        "body": json.dumps({"template": result}),
     }
