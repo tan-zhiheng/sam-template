bdfd94bfc154fdc1c2e1e4dfa456cf6e834bbb2f
tan.zhiheng <tan.zhiheng@tecotec.co.jp>
2023-10-13 10:11:55 +0900

    update cors

--- functions/app2.py
+++ functions/app2.py
@@ -15,5 +15,10 @@ def handler(event, context):
     result = template.render(json.loads(event["body"]))
     return {
         "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Headers": "Content-Type",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "OPTIONS,POST",
+        },
         "body": json.dumps({"template": result}),
     }
