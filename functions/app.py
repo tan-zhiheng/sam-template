2b2826e8a57c165455a340980efb6d6810bc1e31
tan.zhiheng <tan.zhiheng@tecotec.co.jp>
2023-10-13 16:22:48 +0900

    update function

--- functions/app.py
+++ functions/app.py
@@ -1,5 +1,12 @@
 import json
+import time
 
 
 def handler(event, context):
+    start_time = time.time()
     print(json.dumps(event))
+    end_time = time.time()
+    processing_time = end_time - start_time
+    min_wait_time = 1
+    wait_time = max(0, min_wait_time - processing_time)
+    time.sleep(wait_time)
