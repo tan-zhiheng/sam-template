3549113f515a4e06cb95ca99012b0d0ae44fd3a1
tan.zhiheng <tan.zhiheng@tecotec.co.jp>
2024-06-27 15:02:21 +0900

    init commit

---
+++ helloworld/app.py
@@ -0,0 +1,17 @@
+from chalice import Chalice
+
+app = Chalice(app_name="helloworld")
+
+
+@app.route("/")
+def index():
+    return {"hello": "world"}
+
+
+app.debug = True
+
+
+@app.on_sqs_message(queue="my-queue", batch_size=1)
+def handle_sqs_message(event):
+    for record in event:
+        app.log.debug("Received message with contents: %s", record.body)
